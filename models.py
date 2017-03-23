5aec90052120e46614225d9a20ca9a58ff78c332
sriamrutha <amrut.ga1996@gmail.com>
2017-03-23 12:12:33 +0530

    Add files via upload

--- models.py
+++ models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.utils import timezone
+
+
+'''class Post(models.Model):
+    author = models.ForeignKey('auth.User')
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(
+            default=timezone.now)
+    published_date = models.DateTimeField(
+            blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title'''
 
 # Create your models here.
+   class AdditionalInfo(models.Model):
+       internships = models.CharField(max_length=200)
+       Activities = models.CharField(max_length=200)
+       projects = models.CharField(max_length=200)
+       Certificates = models.CharField(max_length=200)
+    
+   
+       rollno = models.foreignkey(PersonalInfo)
+
+       def _str_(self):
+           return self.rollno
