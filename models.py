9d9da213f41d7990c0d34344f780e4ad40cdc558
AbbaraoAnvitha <anvitha.2697@gmail.com>
2017-03-24 06:35:53 +0530

    Add files via upload

--- models.py
+++ models.py
@@ -1,16 +1,14 @@
-from __future__ import unicode_literals
-
 from django.db import models
-from django.contrib.auth.models import User
-from django.utils.translation import ugettext_lazy as _
+from django.core.validators import RegexValidator
+from django.core.validators import EmailValidator
 # Create your models here.
-class RegistrationModel(models.Model):
- 
-    username = models.CharField(max_length=200)
-    password = models.CharField(max_length=30)
-    email = models.CharField(max_length = 100)
-    
-    
+class StudentDetails(models.Model):
+   rollno = models.CharField(max_length=200)
+   firstname = models.CharField(max_length=200)
+   lastname = models.CharField(max_length=200)
 
-def _str_(self):
-    return self.rollno
+   phonenumber = models.CharField(max_length = 10)     
+   email = models.CharField(max_length = 100)
+   dob = models.CharField(max_length = 15)
+   def _str_(self):
+       return self.rollno
--- urls.py
+++ urls.py
@@ -1,13 +1,8 @@
-from django.conf.urls import url, include
-from django.contrib import admin
+from django.conf.urls import url
+
 from . import views
-from django.contrib.auth import views as views
 
 urlpatterns = [
-
-	url(r'^$', views.login, name='login'),
-	url(r'^register/$', views.register, name='register'),
-	url(r'^register/success/$', views.register_success, name='register_success'),
-    	url(r'^accounts/login/$', views.login),
-    	url(r'^logout/$', views.logout_page),
+    url(r'^$', views.index, name='index'),
 ]
+
